2d64f69eed8423799660c60cf66f0121536d10ef
surprisedPikachu69 <arvind7447@gmail.com>
2021-10-14 20:04:11 +0530

    Project aded

---
+++ MyApplication/app/src/main/java/com/example/myapplication/CallRoomActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CallRoomActivity extends  AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.call_room);
+    }
+}
