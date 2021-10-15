6f48d90b525dff5abfb5f87b11b71f9c0a111223
surprisedPikachu69 <arvind7447@gmail.com>
2021-10-15 15:00:52 +0530

    pages for buttons

--- MyApplication/app/src/main/java/com/example/myapplication/JumpActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/JumpActivity.java
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CallRoomActivity extends  AppCompatActivity{
+public class JumpActivity extends  AppCompatActivity{
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_call_room);
+        setContentView(R.layout.activity_jump);
     }
 }
