84c449535dfe1dc5a4798608b6f34f9909351773
surprisedPikachu69 <arvind7447@gmail.com>
2022-04-02 22:05:51 +0530

    sort places based on foul count

--- GFG-Room-Database-master/app/src/main/java/com/gtappdevelopers/gfgroomdatabase/ResultActivity.java
+++ GFG-Room-Database-master/app/src/main/java/com/gtappdevelopers/gfgroomdatabase/ResultActivity.java
@@ -63,16 +63,46 @@ public class ResultActivity extends AppCompatActivity {
         
         for(int i=0; i<arrayList.size(); i++){
             String max="0";
+            int foul_count=0;
             String[] temp = arrayList.get(i);
+            ArrayList<String> temp2 = new ArrayList<>();
+
             for(int j=2; j<5; j++) {
                 if(temp[j].equals("x") || temp[j].equals("X")){
+                    foul_count++;
                     continue;
                 }
                 if (Double.parseDouble(temp[j]) > Double.parseDouble(max)) {
                     max = temp[j];
                 }
             }
-            max_marks.add(new String[]{temp[0], temp[1], max});
+
+            /*for(int j=2; j<5; j++) {
+                if(temp[j].equals("x") || temp[j].equals("X")){
+                    foul_count++;
+                }
+                else{
+                    temp2.add(temp[j]);
+                }
+            }
+
+            if(temp2.isEmpty()){
+                max="0";
+            }
+
+            for(int j=0; j<temp2.size()-1; j++) {
+                for(int k=j; k<temp2.size(); k++){
+                    if(Double.parseDouble(temp2.get(k)) < Double.parseDouble(temp2.get(k+1))){
+                        Collections.swap(temp2, k, k+1);
+                    }
+                }
+            }
+
+            if(!(temp2.isEmpty())){
+                max = temp2.get(0);
+            }*/
+
+            max_marks.add(new String[]{temp[0], temp[1], max, Integer.toString(foul_count)});
         }
         return max_marks;
     }
@@ -87,6 +117,17 @@ public class ResultActivity extends AppCompatActivity {
                 }
             }
         }
+
+        for(int i=0; i<total_marks.size(); i++) {
+            for (int j = 0; j < total_marks.size() - 1; j++) {
+                if (Double.parseDouble(total_marks.get(j)[2]) == Double.parseDouble(total_marks.get(j+1)[2])) {
+                    if(Double.parseDouble(total_marks.get(j)[3]) > Double.parseDouble(total_marks.get(j+1)[3])){
+                        Collections.swap(total_marks, j, j+1);
+                    }
+                }
+            }
+        }
+
 
         for(int i=0; i<total_marks.size(); i++){
             sorted_marks.add(total_marks.get(i));
